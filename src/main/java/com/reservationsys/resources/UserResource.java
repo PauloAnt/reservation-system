555ab537ec0c06515bf30f59e75af8a0e3d86e73
PauloAnt <pauloabf235@gmail.com>
2024-03-25 11:22:18 -0300

    CRUD User

--- src/main/java/com/reservationsys/resources/UserResource.java
+++ src/main/java/com/reservationsys/resources/UserResource.java
@@ -1,15 +1,20 @@
 package com.reservationsys.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.reservationsys.dto.UserDTO;
 import com.reservationsys.entities.User;
@@ -34,6 +39,28 @@ public class UserResource {
 		User user=userService.findById(id);
 		UserDTO userDto = new UserDTO(user);
 		return ResponseEntity.ok().body(userDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO user){
+		User obj = userService.fromDTO(user);
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO obj){
+		User user = userService.fromDTO(obj);
+		user.setId(id);
+		userService.update(user);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/com/reservationsys/services/UserService.java
+++ src/main/java/com/reservationsys/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.reservationsys.dto.UserDTO;
 import com.reservationsys.entities.User;
 import com.reservationsys.repositories.UserRepository;
 import com.reservationsys.services.exception.ObjectNotFoundException;
@@ -26,6 +27,31 @@ public class UserService {
 			throw new ObjectNotFoundException("Objeto não encontrado!");
 		}
 		return user.get();
+	}
+	
+	public User insert(User user) {
+		return service.insert(user);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		service.deleteById(id);
+	}
+	
+	public User update(User user) {
+		User obj = findById(user.getId());
+		updateData(obj, user);
+		return service.save(obj);
+		
+	}
+	
+	private void updateData(User new_user, User user) {
+		new_user.setEmail(user.getEmail());
+		new_user.setUsername(user.getUsername());
+	}
+	
+	public User fromDTO(UserDTO userdto) {
+		return new User(userdto.getId(), userdto.getUsername(), userdto.getEmail(), userdto.getPassword());
 	}
 	
 }
